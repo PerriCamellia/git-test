24cb388cba636d3c1a268a9410b3852150a38d45
Camellia <camellia@gmail.com>
2023-03-27 16:31:53 +0800

    push commit

--- app/src/main/java/com/example/git_test/MainActivity.java
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("hello.git4");
         System.out.println("hot-fix test");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
